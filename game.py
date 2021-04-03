dcf8a2ba01cd97403cf74f10cfee7849aeb007f8
Thomas <53497553+thomasandrew@users.noreply.github.com>
2021-04-03 16:42:23 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random
 
 class GuessNumber:
-  def __init__(self, infinity):
-    self.infinity = infinity
+  def __init__(self, infinite):
+    self.infinity = infinite
 
   def guess(self):
     randomNumber = random.randint(0, 100)
     
-    while self.infinity == 1:
+    while self.infinite == 1:
       number = int(input("Type a number: "))
 
       if randomNumber < number:
